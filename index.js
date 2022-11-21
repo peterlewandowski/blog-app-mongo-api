fec6107cdc0ae96c6aed0a6b61052e47fdd140a5
Peter Lewandowski <mapiotr2010@gmail.com>
2022-11-21 10:40:32 -0500

    MonoClient connect to db

--- index.js
+++ index.js
@@ -10,4 +10,11 @@ app.use(express.json());
 
 app.get("/", (req, res) => res.send("working"));
 
+const client = new MongoClient(process.env.MONGO_URI)
+const database = client.db('blog')
+const posts = database.collection('posts')
+
+client.connect()
+console.log('\x1b[33m Connected to mongo! \x1b[0m')
+
 app.listen(process.env.PORT, () => console.log(`Listening on http://localhost:${process.env.PORT}...`));
