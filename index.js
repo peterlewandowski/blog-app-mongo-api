5ec5742cd14536353b0ad2e00932b194edc4c0be
Peter Lewandowski <mapiotr2010@gmail.com>
2022-11-21 11:28:43 -0500

    post and get

--- index.js
+++ index.js
@@ -1,20 +1,44 @@
 import express from "express";
 import cors from "cors";
 import { MongoClient } from "mongodb";
-import 'dotenv/config'
+import "dotenv/config";
 
 // const PORT = 4040;
 const app = express();
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => res.send("working"));
+// app.get("/", (req, res) => res.send("working"));
 
-const client = new MongoClient(process.env.MONGO_URI)
-const database = client.db('blog')
-const posts = database.collection('posts')
+const client = new MongoClient(process.env.MONGO_URI);
+const database = client.db("blog");
+const posts = database.collection("posts");
 
-client.connect()
-console.log('\x1b[33m Connected to mongo! \x1b[0m')
+client.connect();
+console.log("\x1b[33m Connected to mongo! \x1b[0m");
 
-app.listen(process.env.PORT, () => console.log(`Listening on http://localhost:${process.env.PORT}...`));
+// post - add one post item
+// ---- author, date, text
+
+app.post("/", async (req, res) => {
+  //   const newPost = {
+  //     author: "Jon",
+  //     date: "2022-21-11",
+  //     text: "lorem lorem here",
+  //   };
+  //   console.log("req.body ->", req.body);
+
+  await posts.insertOne(req.body);
+
+  res.send("Post was added :)");
+});
+
+// get - to get all posts
+app.get("/", async (req, res) => {
+  const allPosts = await posts.find().toArray();
+  res.send(allPosts);
+});
+
+app.listen(process.env.PORT, () =>
+  console.log(`Listening on http://localhost:${process.env.PORT}...`)
+);
