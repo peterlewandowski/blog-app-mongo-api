ca7e28b386e788e101d04bd58e4a521d0953edb4
Peter Lewandowski <mapiotr2010@gmail.com>
2022-11-21 10:08:34 -0500

    basics

---
+++ src/dbConnect.js
@@ -0,0 +1,7 @@
+import { MongoClient } from 'mongodb'
+import { uri } from "../secrets.js"
+
+export function dbConnect() {
+    const client = new MongoClient(uri)
+    return client.db("mydatabase")
+}
