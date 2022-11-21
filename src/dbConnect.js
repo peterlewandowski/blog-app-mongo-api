2ad217ef757836e9f8053c0b0b1a748081c0c9d2
Peter Lewandowski <mapiotr2010@gmail.com>
2022-11-21 11:42:02 -0500

    not using dbConnect.js (.env instead)

--- src/dbConnect.js
+++
@@ -1,7 +0,0 @@
-import { MongoClient } from 'mongodb'
-import { uri } from "../secrets.js"
-
-export function dbConnect() {
-    const client = new MongoClient(uri)
-    return client.db("mydatabase")
-}
